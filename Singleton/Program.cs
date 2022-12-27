37ac0fd71aeec28df78aeaef164ba625df430686
keyur-s-simformsolutions~ <keyur.s@simformsolutions.com>
2022-12-27 12:29:29 +0530

    Added singleton design pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singleton fromFacebook = Singleton.GetInstance;
+            fromFacebook.PrintDetails("From Facebook");
+            Singleton fromTwitter = Singleton.GetInstance;
+            fromTwitter.PrintDetails("From Twitter");
+            Console.ReadLine();
+        }
+    }
+}
